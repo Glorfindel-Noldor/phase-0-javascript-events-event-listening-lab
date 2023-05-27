536bfd00f25dfe64305dcb2a9da4f3d87ef10f90
Bogar Martinez <bogar.cm@icloud.com>
2023-05-27 09:09:04 -0500

    button smasher

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-function addingEventListener() {
+const noise = document.getElementById('button')
+function secondArgumentWhatItShouldDo(){
+    alert('what it does')
 }
+noise.addEventListener('click',secondArgumentWhatItShouldDo)
